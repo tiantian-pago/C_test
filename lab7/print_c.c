a4877ad98a8aa7df1731aa6083b4b7408fefdb4e
tiantian-pago <2205855339@qq.com>
2020-04-10 13:16:48 +0800

    print C

---
+++ lab7/print_c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+   printf("Hello C-World!\n");
+   printf("********\n");
+   printf("***\n");
+   printf("*\n");
+   printf("*\n");
+   printf("***\n");
+   printf("********\n");
+
+return 0;
+}
