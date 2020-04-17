2c36c3ab65139a546e444b8b7d3fb20a598c2395
tiantian-pago <2205855339@qq.com>
2020-04-17 09:25:57 +0800

    画圆

---
+++ lab55/circle.c
@@ -0,0 +1,23 @@
+/*
+*画图，学用circle画圆形。
+*/
+
+#include<graphics.h>
+
+int main()
+{
+	int driver,mode,i;
+	float j=1,k=1;
+	driver=VGA;
+	mode=VGAHI;
+	initgraph(&driver,&mode,"");
+	setbkcolor(YELLOW);
+	for(i=0;i<=25;i++)
+	{
+		setcolor(8);
+		circle(310,250,k);
+		k=k+j;
+	j=j+0.3;
+	}
+return 0;
+}
